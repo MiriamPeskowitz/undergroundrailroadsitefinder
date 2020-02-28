8a95360cbd4dce1cfd01a2245a185dbdea7d294d
MiriamPeskowitz <miriam5000@gmail.com>
2020-02-27 22:29:44 -0500

    Add fast_jsonapi gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+	def index
+		users = User.all 
+		render json: UserSerializer.new(users)	
+	end
+
+
+	def show
+		user = User.find_by(id: params[:id])
+		render json: user.to_json(:include => :site)
+		#  render json: UserSerializer.new(user)
+
+	end
+
 end
